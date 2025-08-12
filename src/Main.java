fe3366317bec0e268f307cc8572b50b3a0d8e510
Guilherme Nunes <guilherme.augd@gmail.com>
2025-08-12 14:57:54 -0300

    atribuicao de atributos

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         PersonagemMagico mago = new PersonagemMagico();
+
+        mago.nome = "Gandalf";
+        mago.nivelEnergia = 100;
+        mago.poderMagico = "Magia";
+
+        System.out.println(mago.nome + " Energia: " + mago.nivelEnergia + " Poder: " + mago.poderMagico);
     }
 }

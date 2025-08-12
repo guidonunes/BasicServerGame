7ae27efa29c730f6a4f3531a76cd2efcd46c4404
Guilherme Nunes <guilherme.augd@gmail.com>
2025-08-12 16:10:52 -0300

    implementacao de menu interativo

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        PersonagemMagico personagem = new PersonagemMagico();
         int op;
 
         do {
             System.out.println("Escolha uma opção: \n1-Cadastrar Personagem \n2-Exibir Personagem \n0-Sair");
             op = sc.nextInt();
+
+            switch (op) {
+                case 1:
+                    System.out.println("Digite o nome do personagem:");
+                    String nome = sc.next() + sc.nextLine();
+                    System.out.println("Digite o poder do personagem:");
+                    String poder = sc.next() + sc.nextLine();
+                    System.out.println("Digite o nivel de energia");
+                    int energia = sc.nextInt();
+                    personagem.nome = nome;
+                    personagem.nivelEnergia = energia;
+                    personagem.poderMagico = poder;
+                    break;
+                case 2:
+                    System.out.println("Nome: " + personagem.nome + "\nPoder: " +personagem.poderMagico + "\nNivel de Energia: " + personagem.nivelEnergia);
+                    break;
+                case 0:
+                    System.out.println("Finalizando o programa");
+                default:
+                    System.out.println("Opção inválida");
+
+            }
 
 
         } while(op != 0);
